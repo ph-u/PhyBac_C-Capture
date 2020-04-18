e28b27208e0669096a5cbf485b7e1a877df6d52f
ph-u <51484056+ph-u@users.noreply.github.com>
2020-04-18 11:14:58 +0100

    bib collect

---
+++ code/thesisSec/allBib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Author: 	PokMan Ho (hpokman@connect.hku.hk)
+## Script: 	allBib.sh
+## Desc: 	scavange all bib contacted prior writing up
+## Input: 	bash allBib.sh
+## Output: 	bib bin
+## Arg: 	0
+## Date: 	Apr 2020
+
+a0=`pwd` ## prep for full path for bibWrite.sh
+ls ../../reference/bib/ > thesis.txt
+bibWrite.sh ${a0}/thesis.txt ../../reference/bib/
+rm ${a0}/thesis.txt
+exit

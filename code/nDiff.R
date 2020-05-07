b6336d372fc4c9f3e181067e8ef538d91d403f44
ph-u <hpokman@connect.hku.hk>
2020-05-07 17:47:49 +0100

    crash site investigation: effect of initial condition

--- code/nDiff.R
+++ code/nDiff.R
@@ -3,19 +3,20 @@
 # Author:   PokMan Ho
 # Script:   nDiff.R
 # Desc:   	scan for discrepancies between numerical and analytical model solution scan
-# Input: 	Rscript nDiff.R
+# Input: 	Rscript nDiff.R <run_id> <iniPop>
 # Output: 	../result/discrepancy.csv
-# Arg: 		none
+# Arg: 		2
 # Date: 	Apr 2020
 
 ##### env set-up #####
+args = (commandArgs(T))
 source("func.R")
 rAw = read.csv("../result/maxYield_all.csv", header=T)
 dIff = as.data.frame(matrix(NA,nr=nrow(rAw),nc=4))
 
 ##### discrepancies scan #####
 for(i in 1:nrow(rAw)){
-  pA = ebcData(parameter = unname(rAw[i,1:9]))
+  pA = ebcData(iniPop=as.numeric(args[2]), parameter = unname(rAw[i,1:9]))
   dIff[i,] = pA[nrow(pA),-1]-rAw[i,10:13]
   if(i%%5e3==0){cat(paste0(round(i/nrow(rAw)*100),"% finished\n"))}
 };rm(i)
@@ -24,5 +25,5 @@ for(i in 1:nrow(rAw)){
 colnames(dIff) = paste0(colnames(rAw)[10:13],":num-ana")
 eXport = cbind(rAw[,1:9],dIff)
 cat("exporting result\n")
-write.csv(eXport,"../result/discrepancy.csv", quote = F, row.names = F)
+write.csv(eXport,paste0("../result/discrepancy_",args[1],".csv"), quote = F, row.names = F)
 cat("done\n")

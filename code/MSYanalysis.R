b9d54b34a2fa6f517913b4131075261a6e948b9e
ph-u <51484056+ph-u@users.noreply.github.com>
2020-06-09 10:08:18 +0100

    plots

--- code/MSYanalysis.R
+++ code/MSYanalysis.R
@@ -15,10 +15,10 @@
 a = read.csv("../result/anaIRL.csv", header = T)
 source("func.R")
 ot = "../result/"
-
-##### filter for compariable data set #####
 a.b = a ## create backup in case messed up
 a[a==Inf] = a[a<0] = NA ## exclude meaningless result for later filtering
+
+##### filter for compariable data set #####
 # eXc = c();for(i in 14:21){
 #   eXc = unique(c(eXc, which(is.na(a[,i])))) ## scan for excludable rows
 # };rm(i)
@@ -57,7 +57,7 @@ dev.off()
 # matplot(a2$ePR,a2[,c(10:11)], type = "l", lwd = 5, lty = 1, col = cBp[c(3,2)], xlab = "non-respired phytoplanktonic carbon fraction", ylab = "maximum sustainable yield (gC/(m^3 day))")
 # legend("topleft", inset=c(0,0), legend = c("P only","P+B"), pch = rep(16,2), col = cBp[c(3,2)], bty="n", cex = 1.5)
 
-##### feasibility vs MSY plot #####
+##### yield calculation: feasibility vs MSY plot #####
 a$yield3C = a$eqm3C*a$x
 a$yield4C = a$eqm4C*a$x
 
@@ -88,7 +88,7 @@ rEf = c(1:5,8);for(i in 1:ncol(a0)){for(j in 1:nrow(a0)){
 }
 
 ##### plot distribution of sustainable yield (SY) #####
-{i=1 ## i=c(1:5,8)
+{i=8 ## i=c(1:5,8)
   a0 = as.data.frame(matrix(NA,nc=7, nr=length(unique(a[,i]))))
   colnames(a0) = c(colnames(a)[i], paste0(rep(paste0("SY.",c("05","50","95"),"."),2),c(rep(3,3),rep(4,3))))
   a0[,1] = unique(a[,i])
@@ -105,4 +105,33 @@ rEf = c(1:5,8);for(i in 1:ncol(a0)){for(j in 1:nrow(a0)){
           xlab = colnames(a0)[1], ylab = "sustainable yield 95% distribution (gC/day)", main=paste0("Susteinable Yield 95% distribution across\nparameter range of ",colnames(a0)[1]))
   legend("topleft", inset=c(0,0), legend = c("P only","P+B"), pch = rep(16,2), col = cBp[c(3,2)], bty="n", cex = 1.5)
   dev.off()
+  
+  png(paste0(ot,"MSY_",colnames(a0)[1],"_dLog.png"))
+  matplot(a0[,1],log(a0[,-1]), type = "l",lty = rep(c(3,1,2),2),lwd = 5,col = c(rep(cBp[3],3),rep(cBp[2],3)),
+          xlab = colnames(a0)[1], ylab = "log sustainable yield 95% distribution", main=paste0("Susteinable Yield 95% distribution across\nparameter range of ",colnames(a0)[1]))
+  legend("topleft", inset=c(0,0), legend = c("P only","P+B"), pch = rep(16,2), col = cBp[c(3,2)], bty="n", cex = 1.5)
+  dev.off()
 }
+
+##### plot fesibility vs yield #####
+sy = quantile(c(a$yield3C,a$yield4C), probs = seq(0,1,.1), na.rm = T)
+a0 = as.data.frame(matrix(NA,nc=3,nr=length(sy)))
+colnames(a0) = c("SustainableYield", paste0("feasibility.",c(3,4)))
+a0$SustainableYield = sy
+for(i in 1:nrow(a0)){
+  a0[i,-1] = c(length(which(a$yield3C>=a0[i,1])),length(which(a$yield4C>=a0[i,1])))/nrow(a)
+};rm(i)
+
+##### linear x-scale feasibility vs SY #####
+png(paste0(ot,"MSY_",colnames(a0)[1],"_sy.png"))
+matplot(a0[,1],a0[,-1], type = "l",lty = 1:2,lwd = 4,col = cBp[c(3,2)], ylim = c(0,.15),
+        xlab = "sustainable yield (gC/day)",ylab = "feasibility fraction",main="Fraction of feasibility simulated within parameter space\nagainst sustainable yield")
+legend("topright", inset=c(0,0), legend = c("P only","P+B"), pch = rep(16,2), col = cBp[c(3,2)], bty="n", cex = 1.5)
+dev.off()
+
+##### log x-scale feasibility vs SY #####
+png(paste0(ot,"MSY_",colnames(a0)[1],"_syLog.png"))
+matplot(log(a0[,1]),a0[,-1], type = "l",lty = 1:2,lwd = 4,col = cBp[c(3,2)], ylim = c(0,1),
+        xlab = "log sustainable yield",ylab = "feasibility fraction",main="Fraction of feasibility simulated within parameter space\nagainst sustainable yield")
+legend("topright", inset=c(0,0), legend = c("P only","P+B"), pch = rep(16,2), col = cBp[c(3,2)], bty="n", cex = 1.5)
+dev.off()

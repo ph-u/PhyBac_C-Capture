03dd53c3803be5f5ad65eab6d760a0f1b155550e
ph-u <51484056+ph-u@users.noreply.github.com>
2020-07-12 15:37:32 +0100

    project restructure

--- code/project.sh
+++ code/project.sh
@@ -6,15 +6,19 @@
 # Input 	: `bash project.sh`
 # Output 	: none; see respective scripts
 # Arg 		: 0
-# Date 		: Jun 2020
+# Date 		: Jul 2020
 
 cd `dirname $0` 						## set working directory to `code/`
 rm -r ../result/ 2> tmp 				## clear output folder
 mkdir ../result/ 						## remake `result/` directory
+for i in continuous destructive gRate scenario;do
+	rm ../data/${i}.csv 				## remove intermediate data
+done
 
 Rscript rateDet.R 						## temperature standardisation
-Rscript modIRL.R 						## analytical parameter scan
-Rscript analysis.R 						## model result analysis
+Rscript scenario.R 						## set LHS scenario samples
+python3 destructiveHarvest.py 12000 & 	## numerical yield calculation
+bash continuousHarvest.sh 1 20000 200 	## analytical yield calculation
 
 bash writeREADME.sh 					## write readme for project
 

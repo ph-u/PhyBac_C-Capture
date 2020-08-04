da4eabae9796b506ac5e3c5f36615d89e4e7085c
ph-u <51484056+ph-u@users.noreply.github.com>
2020-08-04 23:38:14 +0100

    graphics

--- code/yieldWrangling.R
+++ code/yieldWrangling.R
@@ -24,12 +24,12 @@ rawL = L
 rawL$x = rawL$x-1
 
 ##### daily yield #####
-a$PoH = a$x*a$c3 ## yield in PoH systems
-a$PBH = a$x*a$c4 ## yield in PBH systems
-d$PoN = (d$c3+d$p3+d$b3-(d$c3[1]+d$p3[1]+d$b3[1]))/d$x ## yield in PoN systems
-d$PBN = (d$c4+d$p4+d$b4-(d$c4[1]+d$p4[1]+d$b4[1]))/d$x ## yield in PBN systems
-L$PoN = (L$c3+L$p3+L$b3-(L$c3[1]+L$p3[1]+L$b3[1]))/L$x ## yield in PoN systems in log time space
-L$PBN = (L$c4+L$p4+L$b4-(L$c4[1]+L$p4[1]+L$b4[1]))/L$x ## yield in PBN systems in log time space
+a$PoC = a$x*a$c3 ## yield in PoH systems
+a$PBC = a$x*a$c4 ## yield in PBH systems
+d$PoD = (d$c3+d$p3+d$b3-(d$c3[1]+d$p3[1]+d$b3[1]))/d$x ## yield in PoN systems
+d$PBD = (d$c4+d$p4+d$b4-(d$c4[1]+d$p4[1]+d$b4[1]))/d$x ## yield in PBN systems
+L$PoD = (L$c3+L$p3+L$b3-(L$c3[1]+L$p3[1]+L$b3[1]))/L$x ## yield in PoN systems in log time space
+L$PBD = (L$c4+L$p4+L$b4-(L$c4[1]+L$p4[1]+L$b4[1]))/L$x ## yield in PBN systems in log time space
 a[,c(10:15)] = d[,c(10:15)] = L[,c(10:15)] = NULL
 for(i in 2:9){ ## bit-wise number handling in python and C cause record slight differences
   a[,i] = round(a[,i],4)

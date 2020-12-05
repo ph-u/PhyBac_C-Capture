342a91fabd20e560c1b8d7a6207f9e26fdd4cac2
ph-u <51484056+ph-u@users.noreply.github.com>
2020-12-05 12:46:38 +0000

    paper graphics

--- code/paper_datAttribute.py
+++ code/paper_datAttribute.py
@@ -38,8 +38,8 @@ pAr = tOtal['parameter'].unique()
 cOl = tOtal.columns[2:7]
 dTl = [[cOl[0],(5,10)],[cOl[1],(7,13)],[cOl[2],(7,14)],[cOl[3],(14,26)],[cOl[4],(14,14)]]
 
-for i in range(len(pAr)-1):
-    for j in range(len(dTl)-1):
+for i in range(len(pAr)):
+    for j in range(len(dTl)):
         iDx = [i,j]
         pltDF = tOtal[tOtal['parameter']==pAr[iDx[0]]]
         p = pltDF[[dTl[iDx[1]][0],'perDay']].boxplot(by=dTl[iDx[1]][0], figsize=dTl[iDx[1]][1], grid=False, rot=90)
@@ -48,10 +48,10 @@ for i in range(len(pAr)-1):
 ##### data source distribution on parameters #####
 gRp = rAw.copy()
 gRp['CitationCode'] = pd.Categorical(gRp.Citation).codes
-gRp.groupby(['CitationCode','Citation']).size().to_csv("../p_paperGraphics/citationREF.csv", header=True) ## citation index reference count table
+gRp.groupby(['CitationCode','Citation']).size().to_csv("../p_paperGraphics/citationREF.csv", header=True, index=False) ## citation index reference count table
 
 for i in gRp.columns[2:7]:
-    gRp.groupby(['CitationCode','parameter',i]).size().to_csv("../p_paperGraphics/countTable_"+i+".csv", header=True) ## use csv as record
+    gRp.groupby(['CitationCode','parameter',i]).size().to_csv("../p_paperGraphics/countTable_"+i+".csv", header=True, index=False) ## use csv as record
     cTable = pd.read_csv("../p_paperGraphics/countTable_"+i+".csv", keep_default_na=False)
     uPara = cTable['parameter'].unique()
     uPylm = cTable[i].unique()
@@ -66,7 +66,57 @@ for i in gRp.columns[2:7]:
                     pp = 'one'
                 mp.savefig("../p_paperGraphics/"+pp+"REF/"+uPara[k]+"_"+i+"_"+uPylm[j]+"_refFreq.pdf", dpi=99)
 
-##### Fig.6 reproduce with top feasibility % (PBC, 0.3%) #####
+##### Fig.6 reproduce with top feasibility % (PBC, 0.3%) [20201204 dumped, use R instead] #####
+dEst = pd.read_csv("../data/destructive_Lin.csv", keep_default_na=False)
+cOnt = pd.read_csv("../data/continuous.csv", keep_default_na=False)
 
+## divide data into 4 systems, filter illogical results (finite positive biomass values)
+PoD = dEst[dEst.columns[0:12]].copy()
+PBD = dEst.drop(dEst.columns[9:12], axis=1).copy()
+PoC = cOnt[cOnt.columns[0:12]].copy()
+PBC = cOnt.drop(cOnt.columns[9:12], axis=1).copy()
+for i in range(PoD.shape[0]):
+    if any(PoD.loc[i,PoD.columns[9:12]]<0) or any(np.isfinite(PoD.loc[i,PoD.columns[9:12]])==False): PoD.loc[i,PoD.columns[9:12]]=np.nan
+    if PBD.loc[i,PBD.columns[9]]<0 or any(PBD.loc[i,PBD.columns[10:12]]<=0) or any(np.isfinite(PBD.loc[i,PBD.columns[9:12]])==False): PBD.loc[i,PBD.columns[9:12]]=np.nan
+    if any(PoC.loc[i,PoC.columns[9:12]]<0) or any(np.isfinite(PoC.loc[i,PoC.columns[9:12]])==False): PoC.loc[i,PoC.columns[9:12]]=np.nan
+    if PBC.loc[i,PBC.columns[9]]<0 or any(PBC.loc[i,PBC.columns[10:12]]<=0) or any(np.isfinite(PBC.loc[i,PBC.columns[9:12]])==False): PBC.loc[i,PBC.columns[9:12]]=np.nan
+    if i%1e4==0: print(str(round(i/PoD.shape[0]*100,1))+"%")
+
+PoD.to_csv("../p_paperGraphics/PoD.csv", header=True, index=False)
+PBD.to_csv("../p_paperGraphics/PBD.csv", header=True, index=False)
+PoC.to_csv("../p_paperGraphics/PoC.csv", header=True, index=False)
+PBC.to_csv("../p_paperGraphics/PBC.csv", header=True, index=False)
+
+## in
+dAta = [pd.read_csv("../p_paperGraphics/"+i+".csv") for i in ["PoD","PBD","PoC","PBC"]]
+
+## calculate yield
+sOurce = ['PoD','PBD','PoC','PBC']
+for i in range(len(dAta)):
+    dAta[i]['Src'] = sOurce[i]
+    if i<2:
+        dAta[i]['yield'] = dAta[i].loc[:,dAta[i].columns[9:12]].sum(axis=1).div(dAta[i]['x'].astype(float))
+    else:
+        dAta[i]['yield'] = dAta[i][dAta[i].columns[9]].mul(dAta[i]['x']).astype(float)
+
+# [[np.nanmin(dAta[i]['yield']),np.nanmax(dAta[i]['yield'])] for i in range(len(dAta))]
+## filter top yield simulations
+refLine = min([sum(np.isfinite(dAta[i]['yield'])) for i in range(len(dAta))])
+sOrt = [dAta[i].sort_values(by='yield',axis=0, ascending=False, inplace=False).head(refLine) for i in range(len(dAta))]
+for i in range(len(dAta)):
+    tmp = list(sOrt[i].columns)
+    tmp[9:12] = ['c','p','b']
+    sOrt[i].columns = tmp
+
+## combine data
+pLot = sOrt[0]
+for i in range(1,4):
+    pLot = pLot.append(sOrt[i], ignore_index=True, sort=False)
+
+## standardise parameter values
+for i in range(9):
+    pLot[pLot.columns[i]] = pLot[pLot.columns[i]].round(4)
+
+pLot.to_csv("../p_paperGraphics/totalPlot.csv", header=True, index=False)
 
 #sys.exit(0)
---
+++ code/paper_plotTopPerc.R
@@ -0,0 +1,41 @@
+#!/bin/env Rscript
+
+# author: ph-u
+# script: paper_plotTopPerc.R
+# desc: plot top percentage yield from P[oB][CD] systems
+# in: Rscript paper_plotTopPerc.R
+# out: none
+# arg: 0
+# date: 20201204
+
+##### in #####
+a = read.csv("../p_paperGraphics/totalPlot.csv", header=T)
+cBp = data.frame(PBC=c("#E69F00","#E69F004D"), PBD=c("#000000","#0000001A"), PoC=c("#009E73","#009E734D"), PoD=c("#D55E00","#D55E004D"))
+
+##### axis labels #####
+axTitle = c(
+  bquote(italic(e[PR]) ~ "( no unit )"),
+  bquote(italic(e[P]) ~ "( no unit )"),
+  bquote(italic(g[P]) ~ "(" ~ day^-1 ~ ")"),
+  bquote(italic(a[P]) ~ "(" ~ m^3*gC^-1*day^-1 ~ ")"),
+  bquote(italic(e[BR]) ~ "( no unit )"),
+  bquote(italic(e[B]) ~ "( no unit )"),
+  bquote(italic(g[B]) ~ "(" ~ m^3*gC^-1*day^-1 ~ ")"),
+  bquote(italic(m[B]) ~ "(" ~ day^-1 ~ ")"),  
+  expression("yield ( gCm"^"-3"*"day"^"-1"*")"),
+  expression("log(yield+1) ( gCm"^"-3"*"day"^"-1"*")")
+)
+
+##### boxplot #####
+pdf("../p_paperGraphics/00topPlot.pdf", width=14, height=18)
+par(mfrow=c(4,2), mar=c(5.5,5,.5,1), cex.lab=1.5, cex.axis=1.5)
+for(i in 2:9){
+    boxplot(log(a$yield+1)~a$Src+a[,i], xaxt="n",xlab="",ylab="", pch=4,border=as.character(cBp[1,]),col=as.character(cBp[2,]))
+    axis(side=1, at=seq(1,length(unique(a[,i]))*length(unique(a$Src)),length(unique(a$Src)))+1.5, labels=round(unique(a[,i])[order(as.numeric(unique(a[,i])))],2), las=2)
+    mtext(axTitle[i-1], side=1, cex=1.5, adj=.49, padj=2+ifelse(i%in%c(2,3,6,7),1,0))
+    mtext(axTitle[10], side=2, cex=1.5, adj=.7, padj=-.9)
+    if(i==2){
+        legend('topleft', ncol=ncol(cBp), bg=rgb(1,1,1,.9), legend=colnames(cBp), pch=16, col=as.character(cBp[1,]), box.col=rgb(1,1,1,1), cex=2)
+    }
+};rm(i)
+invisible(dev.off())
